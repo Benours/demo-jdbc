261389d5ca379b1c9749e279d6c43102ba212901
benours <benjamin.baska@gmail.com>
2021-11-04 10:52:18 +0100

    tp 4 update

--- src/main/java/fr/diginamic/jdbc/dao/TestInsertion.java
+++ src/main/java/fr/diginamic/jdbc/dao/TestInsertion.java
@@ -1,15 +1,19 @@
 package fr.diginamic.jdbc.dao;
 
+import java.util.List;
+
 import fr.diginamic.entites.Fournisseur;
 
 public class TestInsertion {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Fournisseur insert = new Fournisseur(2, "La Maison du Pain");
 //		Fournisseur insertQuote = new Fournisseur(6, "L'Espace Création");
 		
 		FournisseurDaoJdbc fournisseur = new FournisseurDaoJdbc();
+		List<Fournisseur> liste = fournisseur.extraire();
+		Fournisseur insert = new Fournisseur(liste.get(liste.size() - 1).getId() + 1, "La Maison du Pain");
+		System.out.println(liste.get(liste.size() - 1).getId() + 1);
 		fournisseur.insert(insert);
 //		fournisseur.insert(insertQuote);
 
